af8ab805aacccd39a82b6315785f79835e70a672
Hsu Myat <myatsandi8998@gmail.com>
2024-03-05 23:54:19 +0630

    CustomScrollbar Project added

---
+++ Custom_Scrollbar/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CustomScrollbar from "./components/CustomScrollbar";
+
+function App() {
+  return <CustomScrollbar />;
+}
+
+export default App;
---
+++ Custom_Scrollbar/src/components/CustomScrollbar.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const CustomScrollbar = () => {
+  return (
+    <div className="scrollbar-thin scrollbar-thumb-sky-700 scrollbar-track-sky-300 scrollbar-w-6 scrollbar-h-4 overflow-scroll ">
+      <div className="w-screen h-screen flex flex-wrap justify-center items-center">
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 2</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 3</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 4</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 5</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 6</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+        <div
+          className={`p-[20px] m-[20px]  border bg-rose-400 leading-7 w-[700px] `}
+        >
+          <h1>Title 7</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            accusantium cupiditate debitis at similique rerum ab magni ducimus
+            iusto enim ea labore odio vitae, neque sed repudiandae cum doloribus
+            voluptatem?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomScrollbar;
